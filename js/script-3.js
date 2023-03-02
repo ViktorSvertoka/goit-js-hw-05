e571dc939403b74437fa3ec64b7ad6e160253425
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-02 22:22:28 +0200

    Update script-3.js

--- js/script-3.js
+++ js/script-3.js
@@ -1,14 +1,1 @@
-const ADMIN_PASSWORD = 'jqueryismyjam';
 
-let message;
-
-let password = prompt('Введіть пароль');
-
-if (password === null) {
-  message = 'Скасовано користувачем!';
-} else if (password === ADMIN_PASSWORD) {
-  message = 'Ласкаво просимо!';
-} else {
-  message = 'Доступ заборонений, невірний пароль!';
-}
-alert(message);

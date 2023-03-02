50ba1dcdbcd0cc707751400f222c6aed58b5575f
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-02 23:26:00 +0200

    Update script-4.js

--- js/script-4.js
+++ js/script-4.js
@@ -1,30 +1,3 @@
-// # Завдання 4
-
-// Напиши клас `StringBuilder`. На вхід він отримує один параметр - рядок,
-// який записує у властивість `_value`.
-
-// Додай класу наступний функціонал:
-
-// - Геттер `value` - повертає поточне значення поля `_value`
-// - Метод `append(str)` - отримує параметр str (рядок) і додає його в кінець
-//   `_value`
-// - Метод `prepend(str)` - отримує параметр str (рядок) і додає його на початок
-//   `_value`
-// - Метод `pad(str)` - отримує параметр str (рядок) і додає його на початок і в кінець `_value`
-
-// ```js
-// const builder = new StringBuilder(".");
-
-// builder.append("^");
-// console.log(builder.value); // '.^'
-
-// builder.prepend("^");
-// console.log(builder.value); // '^.^'
-
-// builder.pad("=");
-// console.log(builder.value); // '=^.^='
-// ```
-
 class StringBuilder {
   constructor(_value) {
     this._value = _value;
@@ -50,10 +23,10 @@ class StringBuilder {
 const builder = new StringBuilder('.');
 
 builder.append('^');
-console.log(builder.value); // '.^'
+console.log(builder.value);
 
 builder.prepend('^');
-console.log(builder.value); // '^.^'
+console.log(builder.value);
 
 builder.pad('=');
-console.log(builder.value); // '=^.^='
+console.log(builder.value);

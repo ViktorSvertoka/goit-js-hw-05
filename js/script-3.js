bb545f884b21dfe6ef6239ac74c756824efe6b68
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-02 23:10:54 +0200

    Update script-3.js

--- js/script-3.js
+++ js/script-3.js
@@ -1,33 +1,3 @@
-// # Завдання 3
-
-// Напиши клас `Storage`, який буде створювати об'єкти для управління складом
-// товарів. При виклику буде отримувати один аргумент - початковий масив товарів, і
-// записувати його у властивість `items`.
-
-// Додай методи класу:
-
-// - `getItems()` - повертає масив поточних товарів
-// - `addItem(item)` - отримує новий товар і додає його до поточних
-// - `removeItem(item)` - отримує товар і, якщо він є, видаляє його з поточних
-
-// ```js
-// const storage = new Storage([
-//   "Нанітоіди",
-//   "Пролонгер",
-//   "Залізні жупи",
-//   "Антигравітатор",
-// ]);
-
-// const items = storage.getItems();
-// console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
-
-// storage.addItem("Дроїд");
-// console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
-
-// storage.removeItem("Пролонгер");
-// console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
-// ```
-
 class Storage {
   constructor(items) {
     this.items = items;
@@ -55,12 +25,12 @@ const storage = new Storage([
 
 const items = storage.getItems();
 
-console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+console.table(items);
 
 storage.addItem('Дроїд');
 
-console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+console.table(storage.items);
 
 storage.removeItem('Пролонгер');
 
-console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+console.table(storage.items);

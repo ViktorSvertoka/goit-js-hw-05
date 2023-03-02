3a3b6f64f27e9c5832264bd663927d348a219234
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-02 22:53:15 +0200

    Feat(js): added script to task 2 - task done

--- js/script-2.js
+++ js/script-2.js
@@ -1,1 +1,58 @@
+// # Завдання 2
 
+// Напиши клас `User` для створення користувача з наступними властивостями:
+
+// - `name` - рядок
+// - `age` - число
+// - `followers` - число
+
+// Добав метод `getInfo()`, який, виводить рядок:
+// `User ${ім'я} is ${вік} years old and has ${кількість фоловерів} followers`
+
+// ```js
+// const mango = new User({
+//   name: "Mango",
+//   age: 2,
+//   followers: 20,
+// });
+
+// mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+// const poly = new User({
+//   name: "Poly",
+//   age: 3,
+//   followers: 17,
+// });
+
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
+// ```
+
+class User {
+  constructor({ name, age, followers }) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+
+  getInfo() {
+    console.log(
+      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`,
+    );
+  }
+}
+
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  followers: 20,
+});
+
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  followers: 17,
+});
+
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
